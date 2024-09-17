98b2398adbc269ba3df879a0c19e36b7be254589
Soumik Majumder <soumikcode02@gmail.com>
2024-09-17 22:11:53 +0530

    deployment adjustment

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const corsOptions = {
     credentials: true
 }
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
